f2d09fd1ff0a9ee34af5b2983e3197b8c4eae877
Sebas Silva Narro <sebassilvanarro@gmail.com>
2025-02-13 12:27:18 +0100

    temp

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include "cazador.h"
 #include "combate.h"
 #include "dragon.h"
-#include ".h"
+#include "utilidades.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+		
+	return 0;
+}
